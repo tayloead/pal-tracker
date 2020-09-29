1eb6202bb778067ca06845077d2593047bc8ec79
Ubuntu <ubuntu@ip-172-31-84-129.ec2.internal>
2020-09-29 16:31:58 +0000

    configuring an app

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,20 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    String word = "hello";
+
+    public WelcomeController(@Value("${welcome.message}") String a_welcome_message) {
+        word = a_welcome_message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return word;
+    }
 }
